23fffa6d3cd3d49c1293210de3066be949de0df7
aperture-sandi <88592169+aperture-sandi@users.noreply.github.com>
2022-06-26 16:17:07 -0400

    Fixed (18th) to say "current" and added (36th)
    Fixed (18th) to state current instead of previous, added (36th)

--- hooks/_init.sh
+++ hooks/_init.sh
@@ -24,48 +24,55 @@
 # 15th - Active Nominators stashes [stash_1, stash_2, ..] (--expose-nominators flag must be set)
 # 16th - Active Nominators stakes [stake_1, stake_2, ..] (--expose-nominators flag must be set)
 #
-# 17th - Number of Authored blocks (Session - 1) (--expose-authored-blocks flag must be set)
+# 17th - Number of Authored blocks (Current Session) (--expose-authored-blocks flag must be set)
 # 18th - Not applicable
 #
 # 19th - All Nominators stashes [stash_1, stash_2, ..] (--expose-all-nominators flag must be set)
 #
 # > Special character '!' controls message visibility on Matrix (Element)
 # Any message that starts with '!' will be sent to Matrix, to the user private room
-# 
+#
 # echo "! This message will be sent to Matrix"
 # echo "This message will NOT be sent to Matrix"
-# 
+#
 # ***** START *****
 #
-echo "! e.g. Write your own script here"
-echo "! --------------------------------"
-echo "! Positional arguments:"
-echo "! 1st - Stash -> $1" 
-echo "! 2nd - Identity -> $2"
-echo "! 3rd - Queued session keys -> ${3:0:16}.."
-echo "! 4th - Is active? -> $4"
-echo "! 5th - Session keys queued? -> $5"
-echo "! 6th - Era -> $6"
-echo "! 7th - Session -> $7"
-echo "! 8th - Eras session index -> $8"
-echo "! 9th - Current block -> $9"
-echo "! (10th) - Network name -> ${10}"
-echo "! (11th) - Network token symbol -> ${11}"
-echo "! (12th) - Network token decimals -> ${12}"
-echo "! (13th) - Validator total stake -> ${13}"
-echo "! (14th) - Validator own stake -> ${14}"
-echo "! (15th) - Active Nominators -> ${15}"
-echo "! (16th) - Active Nominators Stake -> ${16}"
-echo "! (17th) - Number of Authored blocks in current Session -> ${17}"
-echo "! (18th) - NA"
-echo "! (19th) - All Nominators -> ${19}"
-echo "! -------------------------------"
+echo #" e.g. Write your own script here"
+echo "--------------------------------"
+echo "Positional arguments:"
+echo "1st - Stash -> $1"
+echo "2nd - Identity -> $2"
+echo "3rd - Queued session keys -> ${3:0:16}.."
+echo "4th - Is active? -> $4"
+echo "5th - Session keys queued? -> $5"
+echo "6th - Era -> $6"
+echo "7th - Session -> $7"
+echo "8th - Eras session index -> $8"
+echo "9th - Current block -> $9"
+echo "(10th) - Network name -> ${10}"
+echo "(11th) - Network token symbol -> ${11}"
+echo "(12th) - Network token decimals -> ${12}"
+echo "(13th) - Projected APR -> ${13}"
+echo "(14th) - Validator total stake -> ${14}"
+echo "(15th) - Validator own stake -> ${15}"
+echo "(16th) - Active Nominators -> ${16}"
+echo "(17th) - Active Nominators Stake -> ${17}"
+echo "(18th) - Number of Authored blocks in current Session -> ${18}"
+echo "(19th) - Number of Authored blocks in previous 6 Sessions -> ${19}"
+echo "(20th) - All Nominators -> ${20}"
+echo "(21th) - NA"
+echo "(22th) - Is Para Validator? -> ${22}"
+echo "(23th) - Number of Para Validator times in previous 6 Sessions -> ${23}"
+echo "(24th) - Last era validator points -> ${24}"
+echo "(25th) - Last era avg validators points -> ${25}"
+# Leave room to stay compatible with future turboflakes additions...
+echo "(36th) - Current era validator points -> ${36}"
+echo "-------------------------------"
 if [ "$4" = "true" ]
 then
-  echo "! 🟢 -> 😎"
+  echo " 🟢 -> 😎"
 else
-  echo "! 🔴 -> 😤"
+  echo " 🔴 -> 😤"
 fi
 #
 # ***** END *****
-
